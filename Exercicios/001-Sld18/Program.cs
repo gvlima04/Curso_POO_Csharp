cbd290d3e0cd3e4c37d6d938931346dcbd7fa3a1
Guilherme V. Lima <guilhermevlima04@outlook.com>
2021-10-23 22:06:45 -0300

    Exercício 1

---
+++ Exercicios/001-Sld18/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace _001_Sld18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string prod1 = "Computador";
+            string prod2 = "Mesa";
+            string moeda = "R$";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.00;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("\nProdutos:");
+            Console.WriteLine($"{prod1}, cujo o preço é {moeda} {preco1:F2}");
+            Console.WriteLine($"{prod2}, cujo o preço é {moeda} {preco2:F2}");
+
+            Console.WriteLine($"\nRegistro: {idade} anos de idade, código {codigo} e gênero: {genero}\n");
+
+            Console.WriteLine($"Medida com 8 casas decimais: {medida:F8}");
+            Console.WriteLine($"Arredondando para 2 casas decimais {medida:F2}");
+            Console.WriteLine("Utilizando o invariant culture " + medida.ToString("F2",CultureInfo.InvariantCulture)+"\n");
+            
+        }
+    }
+}
