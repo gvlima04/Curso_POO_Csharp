85d562817498ee3fdc4034cc460da3f2f4e9abf3
Guilherme V. Lima <guilhermevlima04@outlook.com>
2021-10-23 21:32:14 -0300

    Initial commit

---
+++ 001-HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿//using System; //Bibilioteca com funções básicas do C#
+
+namespace _001_HelloWorld //Declarando que o programa está dentro desse namespace
+{
+    class Program //Classe (todo código deve estar dentro de uma classe)
+    {
+        static void Main(string[] args) //Método (membro da classe)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
