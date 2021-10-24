09c5118569a63dadd0cfd0254c345cfb4b12f3f6
Guilherme V. Lima <guilhermevlima04@outlook.com>
2021-10-23 23:14:23 -0300

    Exercício 2

---
+++ Exercicios/002-Sld23/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace _002_Sld23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nome completo:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Número da casa:");
+            int numQuartos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor da conta de energia:");
+            double valEnergia = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escreva seu último nome, idade e altura");
+            string[] dados = Console.ReadLine().Split(' ');
+
+            string lastName = dados[0];
+            int idade = int.Parse(dados[1]);
+            double alt = double.Parse(dados[2]);
+
+            Console.WriteLine(nome);
+            Console.WriteLine(numQuartos);
+            Console.WriteLine(valEnergia.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(lastName);
+            Console.WriteLine(idade);
+            Console.WriteLine(alt.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
