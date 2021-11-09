a6624c956e0d14f2cea1fc9b334606078f5984e7
Guilherme V. Lima <guilhermevlima04@outlook.com>
2021-11-08 22:07:41 -0300

    Tipos referência e structs; Desalocação de memória (Garbage Collector e Escopo); Nullable; Vetores

---
+++ Exercicios/007_Sld69_Vet/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _007_Sld69_Vet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Número de inserções:");
+            int num = int.Parse(Console.ReadLine());
+
+            double[] alturas = new double[num];
+
+            for (int i = 0; i < alturas.Length; i++)
+            {
+                System.Console.Write("Altura " + (i+1) + ": ");
+                alturas[i] = double.Parse(Console.ReadLine());
+            }
+
+            double soma = 0.0;
+
+            for (int i = 0; i < alturas.Length; i++)
+            {
+                soma += alturas[i];
+            }
+
+            double media = soma/num;
+
+            System.Console.WriteLine("A média das alturas é: " + media.ToString("F2"));
+        }
+    }
+}
