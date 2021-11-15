e3f9296dd3f2737baf796f3a7aaab8d78166aeea
Guilherme V Lima <guilhermevlima04@outlook.com>
2021-11-15 00:09:19 -0300

    Herança; Upcasting e Downcasting; virtual & override; classes e métodos selados; polimorfismo; classes e métodos abstratos

---
+++ Exercicios/010_Sld131_Heranca_Polimorf/Sld131/Sld131/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,18 @@
+﻿namespace Sld131.Entities
+{
+    class OutsourcedEmployee : Employee
+    {
+        public double AddtionalCharge { get; set; }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double addtionalCharge)
+            : base(name, hours, valuePerHour)
+        {
+            AddtionalCharge = addtionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + AddtionalCharge * 1.1;
+        }
+    }
+}
---
+++ Exercicios/010_Sld131_Heranca_Polimorf/Sld131/Sld131/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Sld131.Entities;
+
+namespace Sld131
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of employees: ");
+            int numEmployees = int.Parse(Console.ReadLine());
+
+            List<Employee> list = new List<Employee>();
+
+            for (int i = 1; i <= numEmployees; i++)
+            {
+                Console.WriteLine($"Employee #{i}:");
+
+                Console.Write($"Outsourced (y/n)? ");
+                string outsourced = Console.ReadLine();
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine());
+
+                if (outsourced.ToLower() == "y")
+                {
+                    Console.Write("Additional charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine());
+
+                    list.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+
+                Console.WriteLine("");
+
+            }
+
+            Console.WriteLine("Payments:");
+            foreach (Employee employee in list)
+            {
+                Console.WriteLine(employee.Name + " - $" + employee.Payment());
+            }
+
+        }
+    }
+}
