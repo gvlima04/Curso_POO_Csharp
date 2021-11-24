108e9db0c3548e9d5ab09ac9d9de253a63514b62
Guilherme V Lima <guilhermevlima04@outlook.com>
2021-11-24 08:28:59 -0300

    Interface IComparable, Extension Methods, Expressões Lambda e programação funcional, delegates

---
+++ Exercicios/014_Sld212_Set/Set/Set/Entities/Student.cs
@@ -0,0 +1,39 @@
+﻿namespace Set.Entities
+{
+
+    class Student
+    {
+        public int Id { get; private set; }
+        public string Name { get; set; }
+
+        public Student(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public Student(int id)
+        {
+            Id = id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Student))
+            {
+                return false;
+            }
+
+            Student other = obj as Student;
+
+            return Id.Equals(other.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+
+    }
+}
---
+++ Exercicios/014_Sld212_Set/Set/Set/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Set.Entities;
+
+namespace Set
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Student> set = new HashSet<Student>();
+
+            Console.Write("How many courses: ");
+            int numberOfCourses = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numberOfCourses; i++)
+            {
+                Console.Write($"Number of Students for course {i}: ");
+                int numStudents = int.Parse(Console.ReadLine());
+
+                for (int j = 1; j <= numStudents; j++)
+                {
+                    Console.Write($"Student Id: ");
+                    int id = int.Parse(Console.ReadLine());
+
+
+                    set.Add(new Student(id));
+
+                    Console.WriteLine("");
+                }
+
+            }
+
+            Console.WriteLine($"Total students: {set.Count}");
+        }
+    }
+}
